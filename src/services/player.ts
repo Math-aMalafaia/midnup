da410d13268b657d5dbbb95ca6e004f2c52e9a8a
Math.aMalafaia <math.amalafaia@gmail.com>
2026-09-01 08:10:25 -0300

    Feat: adição do banco de dados, para a inplementação

---
+++ src/services/player.ts
@@ -0,0 +1,23 @@
+import { supabase } from '../lib/supabase'
+
+export async function getPlayerProfile() {
+  const {
+    data: { user },
+  } = await supabase.auth.getUser()
+
+  if (!user) {
+    return null
+  }
+
+  const { data, error } = await supabase
+    .from('profiles')
+    .select('*')
+    .eq('id', user.id)
+    .single()
+
+  if (error) {
+    throw error
+  }
+
+  return data
+}
